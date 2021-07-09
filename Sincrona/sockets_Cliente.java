74dfc025a9ae06001b565ef85b7c123407fee007
leonardo-fabricio <leonardo.fabricio@escolar.ifrn.edu.br>
2021-07-09 10:28:42 -0300

    Comunicação sincrona

---
+++ Sincrona/sockets_Cliente.java
@@ -0,0 +1,51 @@
+package Sincrona;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author leonn
+ */
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintStream;
+import java.net.*;
+import java.net.Socket;
+import java.util.Date;
+import java.util.NoSuchElementException;
+import java.net.ConnectException;
+
+import java.util.Scanner;
+public class sockets_Cliente {
+    public static void main(String[] args) throws ClassNotFoundException{
+        try{
+            Socket cliente = new Socket("127.0.0.1",12345);
+            System.out.println("Conexao feita");
+            
+            Scanner leDoSocketServidor = new Scanner(cliente.getInputStream());
+            PrintStream envia = new PrintStream (cliente.getOutputStream());
+            Scanner t = new Scanner(System.in);
+            System.out.print("ESCREVA: ");
+            while(t.hasNextLine()){
+                envia.println(t.nextLine());
+                System.out.println("\nRESPOSTA: "+leDoSocketServidor.nextLine());
+                System.out.print("ESCREVA: ");
+                
+            }
+        }catch(UnknownHostException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        
+    }
+}
+//            RECEBE DATA E HORA DA CONEXAO DO SERVIDOR
+//            ObjectInputStream in = new ObjectInputStream(cliente.getInputStream());
+//            String objt = (String)entrada.readObject();;
+//            System.out.println(data_atual.toString());
---
+++ Sincrona/sokets_Servidor.java
@@ -0,0 +1,94 @@
+package Sincrona;
+
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author leonn
+ */
+public class sokets_Servidor {
+    public static void main(String[] args) {
+	try{
+            System.out.println("iniciando o servidor...");
+            ServerSocket servidor = new ServerSocket(12345);
+            System.out.println("O servidor passa a aguardar conexoes...");
+            Socket o_cliente = servidor.accept();
+            System.out.println("Cliente "+o_cliente.getInetAddress().getHostAddress()+" conectado!");
+            //Thread.sleep(2000);
+            
+            PrintStream devolve = new PrintStream (o_cliente.getOutputStream());
+            Scanner leDoSocketCliente = new Scanner(o_cliente.getInputStream());
+            while(leDoSocketCliente.hasNextLine()){
+                    System.out.println("RESPOSTA: "+leDoSocketCliente.nextLine());
+                    System.out.print("ESCREVA: ");
+                    Scanner t = new Scanner(System.in);
+                    devolve.println(t.nextLine());
+            }
+            leDoSocketCliente.close();
+            leDoSocketCliente = null;
+            o_cliente.close();
+            o_cliente = null;
+            servidor.close();
+            servidor = null;
+        } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        }
+	}
+}
+//public static void main(String[] args) throws IOException {
+//        try{
+//            System.out.println("Iniciando");
+//            ServerSocket sv = new ServerSocket(54321);
+//            Socket cliente = sv.accept();
+//
+//            System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
+//
+//            PrintStream out = new PrintStream(cliente.getOutputStream()); //ENVIA DADOS
+//            Scanner in = new Scanner(cliente.getInputStream()); //RECEBE DADOS DO CLIENTE
+//
+//
+//            Scanner scan = new Scanner(System.in);
+//            while(scan.hasNextLine()){
+//                System.out.println(in.nextLine());
+//                out.println(scan.hasNextLine());
+//            }
+//        }catch(NoSuchElementException e){
+//            e.printStackTrace();
+//        }
+//    }
+
+
+///////////////////////
+//System.out.println("iniciando o servidor...");
+//ServerSocket servidor = new ServerSocket(12345);
+//System.out.println("O servidor passa a aguardar conexoes...");
+//Socket o_cliente = servidor.accept();
+//System.out.println("Cliente "+o_cliente.getInetAddress().getHostAddress()+" conectado!");
+////Thread.sleep(2000);
+//
+//Scanner leDoSocketCliente = new Scanner(o_cliente.getInputStream());
+//while(leDoSocketCliente.hasNextLine()){
+//        System.out.println(leDoSocketCliente.nextLine());				
+//}
+//leDoSocketCliente.close();
+//leDoSocketCliente = null;
+//o_cliente.close();
+//o_cliente = null;
+//servidor.close();
+//servidor = null;
